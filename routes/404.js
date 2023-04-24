82d2842da0b0c95d9487357c101b7b00a78cf0bd
nhamonin <nazarii.hamonin@gmail.com>
2023-04-24 13:11:38 +0300

    Add prettier as a dev dependency to package.json and update files formatting in various files

--- routes/404.js
+++ routes/404.js
@@ -3,7 +3,7 @@ import express from 'express';
 const router = express.Router();
 
 router.use('/', (req, res, next) => {
-    res.status(404).render('404', { pageTitle: 'Page Not Found' });
+  res.status(404).render('404', { pageTitle: 'Page Not Found' });
 });
 
 export default router;
--- routes/admin.js
+++ routes/admin.js
@@ -5,7 +5,10 @@ const router = express.Router();
 const products = [];
 
 router.get('/add-product', (req, res, next) => {
-  res.render('add-product', { pageTitle: 'Add Product', path: '/admin/add-product' });
+  res.render('add-product', {
+    pageTitle: 'Add Product',
+    path: '/admin/add-product',
+  });
 });
 
 router.post('/add-product', (req, res, next) => {
@@ -15,5 +18,5 @@ router.post('/add-product', (req, res, next) => {
 
 export default {
   routes: router,
-  products
-}
+  products,
+};
