e843509ecdcc2abc3b01588dfb1249c08a481437
nhamonin <nazarii.hamonin@gmail.com>
2023-04-24 15:24:22 +0300

    Update package.json and shop.js to use the latest versions of nodemon, ejs, express-handlebars, and pug, and refactor shop.pug to use the updated variable name "products" instead of "prods".

--- routes/shop.js
+++ routes/shop.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 router.get('/', (req, res, next) => {
   const products = adminData.products;
-  res.render('shop', { prods: products, pageTitle: 'Shop', path: '/' });
+  res.render('shop', { products, pageTitle: 'Shop', path: '/' });
 });
 
 export default router;
