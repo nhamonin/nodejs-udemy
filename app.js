a8cd5af493601cc09b0939062946fce4f7c79847
nhamonin <nazarii.hamonin@gmail.com>
2023-04-25 13:41:34 +0300

    sync with author of the course; switch to from module to commonJS

--- app.js
+++ app.js
@@ -1,18 +1,24 @@
-import express from 'express';
-import bodyParser from 'body-parser';
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const errorController = require('./controllers/error');
 
 const app = express();
 
 app.set('view engine', 'pug');
 app.set('views', 'views');
 
-import mainRoute from './routes/main.js';
-import usersData from './routes/users.js';
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(mainRoute);
-app.use(usersData.routes);
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
 
-app.listen(3003);
+app.use(errorController.get404);
+
+app.listen(3000);
