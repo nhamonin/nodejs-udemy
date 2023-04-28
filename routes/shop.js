ba2fc27ca5d4390277a65f340a45d8f4dff639f2
nhamonin <nazarii.hamonin@gmail.com>
2023-04-28 15:37:09 +0300

    Add mobile navigation functionality and side menu toggle button to main header

--- routes/shop.js
+++ routes/shop.js
@@ -5,9 +5,9 @@ const shopController = require('../controllers/shop');
 const router = express.Router();
 
 router.get('/', shopController.getIndex);
-router.get('/products', shopController.getProducts);
 router.get('/cart', shopController.getCart);
 router.get('/checkout', shopController.getCheckout);
 router.get('/orders', shopController.getOrders);
+router.get('/products', shopController.getProducts);
 
 module.exports = router;
