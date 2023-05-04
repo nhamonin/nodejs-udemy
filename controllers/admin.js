d5972fd60fb797cde19dd71f4eaed3679d95241c
nhamonin <nazarii.hamonin@gmail.com>
2023-05-04 21:01:04 +0300

    Add userId field to Product model and set it to the id of the authenticated user when a new product is added

--- controllers/admin.js
+++ controllers/admin.js
@@ -20,7 +20,7 @@ exports.getAddProduct = (req, res, next) => {
 
 exports.postAddProduct = async (req, res, next) => {
   const { title, imageUrl, price, description } = req.body;
-  const product = new Product({ title, price, imageUrl, description });
+  const product = new Product({ title, price, imageUrl, description, userId: req.user });
   product
     .save()
     .then(() => {
