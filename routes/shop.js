7492753553f4c076b12da76491f686c4d7af5144
nhamonin <nazarii.hamonin@gmail.com>
2023-05-06 20:57:10 +0300

    Add authentication middleware to protect routes in admin.js and shop.js, mobile menu bugfix

--- routes/shop.js
+++ routes/shop.js
@@ -1,17 +1,18 @@
 const express = require('express');
 
 const shopController = require('../controllers/shop');
+const isAuth = require('../middleware/is-auth');
 
 const router = express.Router();
 
 router.get('/', shopController.getIndex);
-router.get('/cart', shopController.getCart);
-router.post('/cart', shopController.postCart);
-router.get('/checkout', shopController.getCheckout);
-router.get('/orders', shopController.getOrders);
+router.get('/cart', isAuth, shopController.getCart);
+router.post('/cart', isAuth, shopController.postCart);
+router.get('/checkout', isAuth, shopController.getCheckout);
+router.get('/orders', isAuth, shopController.getOrders);
 router.get('/products/:productId', shopController.getProductDetails);
 router.get('/products', shopController.getProducts);
-router.post('/cart-delete-product', shopController.postCartDeleteProduct);
-router.post('/create-order', shopController.postOrder);
+router.post('/cart-delete-product', isAuth, shopController.postCartDeleteProduct);
+router.post('/create-order', isAuth, shopController.postOrder);
 
 module.exports = router;
